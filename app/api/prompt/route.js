e68cf582646652ed345e04a644969785681fe35b
MeldinRadoncic <meldinradoncic89@gmail.com>
2023-06-14 11:41:38 -0400

    Fix missing closing parentheses and brace in GET method

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -3,18 +3,17 @@ import { connectToDatabase } from "@utils/database";
 import Prompt from "@models/Prompt";
 
 export const GET = async (req) => {
-    try {
-        await connectToDatabase();
-        const prompts = await Prompt.find({}).populate("creator")
-       return new Response(JSON.stringify(prompts), {
-        status: 200,
-       });
-    } catch (error) {
-        console.log(error);
-       return new Response("Failed to Load", {
-        status: 500,
-    }
-)}
+  try {
+    await connectToDatabase();
+    const prompts = await Prompt.find({}).populate("creator");
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Failed to Load", {
+      status: 500,
+    });
+  }
 };
-
 
